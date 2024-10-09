4b004f83d53b381f030c5a2e1b73b2d1ae2b5e47
Olga Bachurina <bachuri12@ukr.net>
2024-10-09 15:49:33 +0300

    Добавлена поддержка клиентской части на React в Program.cs.
    - Настроено middleware для обслуживания статических файлов React.
    - Реализована интеграция между клиентской частью (React) и сервером (ASP.NET).
    - Обеспечена обработка API-запросов от клиентского приложения.
    
    Это изменение позволяет взаимодействовать с React-приложением через ASP.NET, улучшая работу с фронтендом.

--- Program.cs
+++ Program.cs
@@ -48,7 +48,15 @@ builder.Services.AddScoped<ISongRepository, SongRepository>();
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
